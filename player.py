5b98ac04767e54b364edd2a49254657f207fcd8c
MathInfo1 <168642711+MathInfo1@users.noreply.github.com>
2026-07-24 20:47:28 +0200

    Added initial player files

---
+++ player.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Player():
+    def __init__(self, window, position, face):
+        self.window = window
+        self.position = position
+        self.face = face
+
+    def draw(self):
+        pass
+
+    def move(self, w, a, s, d):
+        pass
---
+++ player_test.py
@@ -0,0 +1,21 @@
+import pygame
+from player import Player
+
+pygame.init()
+face1 = pygame.image.load("images/face1.jpg")
+screen = pygame.display.set_mode((320, 320))
+player = Player(screen, pygame.Vector2(50, 50), "Invisibility")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    screen.fill("grey")
+    player.draw()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
