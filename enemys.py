41637b36d19a0ad8d658900967fd7757f46f9abf
AJGecko <easy.not@gmx.net>
2026-07-24 20:43:43 +0200

    Add initial README and enemy management module

---
+++ enemys.py
@@ -0,0 +1,25 @@
+def init():
+    global enemys
+    enemys = []
+
+class enemy:
+    def __init__(self,x,y,type, id):
+        self.x = x
+        self.y = y
+        self.type
+        self.id = id
+    def update():
+        pass
+    def draw():
+        pass
+
+def spawn(x,y,type):
+    global enemys
+    tempenemy = enemy(x,y,type)
+    enemys.append(tempenemy)
+
+def del(id):
+    global enemys
+    for en in enemys:
+        if en.id == id:
+            enemys.remove(en)
