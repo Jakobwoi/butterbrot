1aabaad075f1c2b4bd4969d6dee180f0dcc6f318
Jakob Woittennek <jakob@woittennek.net>
2026-07-25 14:07:32 +0200

    Refactor enemy class and spawn function

--- enemys.py
+++ enemys.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import essentials as es
 
 enemy_types = {"enemy0": 10, "enemy1": 11, "enemy2": 12}
 
@@ -8,21 +9,23 @@ def init():
     enemys = dict()
 
 class enemy:
-    def __init__(self,x,y,type, id):
-        self.x = x
-        self.y = y
+    def __init__(self, window, pos, health, type, id):
+        self.window = window
+        self.pos = pos
+        self.health = health
         self.type = type
         self.id = id
-    def update():
+    def update(self):
         pass
-    def draw():
-        pass
+    def draw(self):
+        sprite = es.load_sprite(self.type)
+        self.window.blit(sprite, self.pos)
 
-def spawn(x,y,type):
+def spawn(window, pos, health, type):
     global enemys
     enemy_id = int(str(enemy_types[type]) + ("{:02d}".format(random.randint(0, 99)))) # player id is 4 digits, first 2 digits are type, last 2 are random
     print("Spawning enemy with id: " + str(enemy_id))
-    tempenemy = enemy(x,y,type, enemy_id)
+    tempenemy = enemy(window, pos, health, type, enemy_id)
     enemys[enemy_id] = tempenemy
 
 def delete(id):
